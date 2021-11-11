56bdbe3564669c9be45c5dfb94efbc27eb15de4f
jacobwachtel <jawachtel@gmail.com>
2021-11-11 10:20:49 -0500

    Added comments, updated certain sections of code and modified to make fetch quicker

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
+// Clicking the button sets everything in motion.
 document.getElementById('click').onclick = () => {
    let city = document.getElementById('city-input').value;
    searchCity(city);
 };
 
+// Searches weather based on city input.
 const searchCity = (city) => {
    document.getElementById('city-input').value = '';
    document.getElementById('city-name').innerHTML = city;
@@ -12,10 +14,14 @@ const searchCity = (city) => {
    const URL = 'https://api.openweathermap.org/data/2.5/weather';
    let full_Url = `${URL}?q=${city}&units=imperial&appid=${WEATHER_API_KEY}`;
 
+   // fetches weather data from Openweatherapi
    fetch(full_Url)
       .then((response) => {
          return response.json();
       })
+
+      // Gets weather data, destructures, and sets inner html to
+      // current weather
       .then((data) => {
          let { temp } = data.main;
          let { temp_max } = data.main;
@@ -28,37 +34,55 @@ const searchCity = (city) => {
          document.getElementById('weather-type').innerHTML = description;
          getUnsplashPicture(city);
       })
+
+      // If error, lets the user know city is not found. I clear the console
+      // to help preserve API key
       .catch((error) => {
          document.getElementById('weather-type').innerHTML =
-            "I'm sorry, there seems to be a problem locating the weather for this city";
+            'Weather not found.';
          if (error.response && error.response.status === 404) {
             console.clear();
          }
       });
 };
 
+// fetches an object of pictures from Unsplash.com based on
+// the city the user searches for. The sets the background
+// image to that picture.
+
 const getUnsplashPicture = (city) => {
    const API_KEY = PICTURE_API_KEY;
    const URL = `https://api.unsplash.com/search/photos?query=${city}&client_id=${API_KEY}`;
 
+   // fetch picture from unsplash
    fetch(URL)
       .then((response) => {
          return response.json();
       })
+
+      // Gets the pictures (unsplash returns an object of 10 pictures). I
+      // set the picture as a new background image.
       .then((data) => {
+         console.log(data);
+         let { results } = data;
          let img = new Image();
          let pictureImg = document.querySelector('.hero-img-containor');
-         img.src = data.results[getRandomPicture].urls.full;
+         img.src = results[getIndex()].urls.regular;
          pictureImg.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${img.src}`;
       })
+
+      // If error, I set the background to the same image as when
+      // it first loaded. Who doesn't love a good beach picture?
       .catch((error) => {
          let img = new Image();
          let pictureImg = document.querySelector('.hero-img-containor');
-         img.src = 'https://unsplash.com/photos/8Ogfqvw15Rg';
+         img.src = './assets/beach-unsplash.jpg';
          document.getElementById('city-name').innerHTML = 'City Not Found';
          pictureImg.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${img.src}`;
       });
 };
-const getRandomPicture = () => {
-   return Math.floor(Math.random() * 10) + 1;
+
+// Gets a random number, becomes the index for results object above.
+const getIndex = () => {
+   return Math.floor(Math.random() * 5) + 1;
 };
